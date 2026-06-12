f1f290d8642ba6489f81b8587c18903b665d5b81
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 15:42:43 +0300

    Controller

---
+++ src/Controller/Form.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\References\Elements\Controller;
+
+use Ge;
+use Ge\Mvc\Module\BaseModule;
+use Ge\Panel\Widget\EditWindow;
+use Ge\Panel\Controller\FormController;
+
+/**
+ * Контроллер формы элемента интерфейса.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\References\Elements\Controller
+ * @since 1.0
+ */
+class Form extends FormController
+{
+    /**
+     * {@inheritdoc}
+     * 
+     * @var BaseModule|\Rg\Backend\Elements\Desk\Extension
+     */
+    public BaseModule $module;
+
+    /**
+     * Возвращает все свойства элементов интерфейса.
+     * 
+     * @return array
+     */
+    protected function getSourceConfig(): array
+    {
+        $config = $tags = [];
+
+        /** @var null|\Rg\Backend\References\Properties\Extension $extension */
+        $extension = Ge::$app->extensions->create('rg.be.references.properties');
+        if ($extension) {
+            /** @var null|\Rg\Backend\References\Properties\Model\Property $properties */
+            $properties = $extension->getModel('Property');
+            if ($properties) {
+                $rows = $properties->fetchAll();
+                foreach ($rows as $row) {
+                    $property = $row['property'];
+                    $config[$property] = [
+                        'displayName' => $property,
+                        'tooltip'     => $extension->t($row['name'] ?? $property),
+                        'type'        => $row['type'] ?? 'string',
+                        'editor'      => empty($row['editor']) ? null : json_decode($row['editor'])
+                    ];
+
+                    $tags[] = [$property, $property];
+                }
+            }
+        }
+        return [$config, $tags];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createWidget(): EditWindow
+    {
+        /** @var EditWindow $window */
+        $window = parent::createWidget();
+
+        /** @var array $sourceConfig */
+        $sourceConfig = $this->getSourceConfig();
+
+        // панель формы (Ge.view.form.Panel GeJS)
+        $window->form->router->route = $this->module->route('/form');
+        $window->form->loadJSONFile('/form', 'items', [
+            '@sourceConfig' => $sourceConfig[0],
+            '@tags'         => $sourceConfig[1]
+            
+        ]);
+
+        // окно компонента (Ext.window.Window Sencha ExtJS)
+        $window->width = 500;
+        $window->height = 600;
+        $window->responsiveConfig = [
+            'height < 600' => ['height' => '99%'],
+        ];
+        $window->resizable = false;
+        $window->layout = 'fit';
+        $window
+            ->setNamespaceJS('Rg.be.references.elements')
+            ->addRequire('Rg.be.references.elements.PropertiesController' . (RG_DEBUG ? '-debug' : ''))
+            ->addRequire('Ge.view.form.field.Field')
+            ->addRequire('Ge.view.grid.property.Grid');
+        return $window;
+    }
+}
---
+++ src/Controller/Grid.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\References\Elements\Controller;
+
+use Ge;
+use Ge\Mvc\Module\BaseModule;
+use Ge\Panel\Widget\TabGrid;
+use Ge\Panel\Helper\ExtGrid;
+use Ge\Panel\Helper\HtmlGrid;
+use Ge\Panel\Controller\GridController;
+use Ge\Panel\Helper\HtmlNavigator as HtmlNav;
+
+/**
+ *  Контроллер списка элементов интерфейса.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\References\Elements\Controller
+ * @since 1.0
+ */
+class Grid extends GridController
+{
+    /**
+     * {@inheritdoc}
+     * 
+     * @var BaseModule|\Rg\Backend\References\Elements\Extension
+     */
+    public BaseModule $module;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createWidget(): TabGrid
+    {
+        /** @var TabGrid $tab Сетка данных (Ge.view.grid.Grid GeJS) */
+        $tab = parent::createWidget();
+
+        // столбцы (Ge.view.grid.Grid.columns GeJS)
+        $tab->grid->columns = [
+            ExtGrid::columnNumberer(),
+            ExtGrid::columnAction(),
+            [
+                'xtype'     => 'templatecolumn',
+                'text'      => ExtGrid::columnInfoIcon($this->t('Name')),
+                'dataIndex' => 'name',
+                'cellTip'   => HtmlGrid::tags([
+                    HtmlGrid::header('{nameLo}'),
+                    HtmlGrid::fieldLabel($this->t('Name'), '{name}'),
+                    HtmlGrid::fieldLabel(
+                        $this->module->t('Name') . ' (' . Ge::$app->language->name . ')', 
+                        '{nameLo}'
+                    ),
+                    HtmlGrid::fieldLabel($this->t('Type'), '{type}'),
+                    HtmlGrid::fieldLabel($this->t('Form field'), HtmlGrid::tplChecked('isField')),
+                    HtmlGrid::fieldLabel($this->t('Enabled'), HtmlGrid::tplChecked('enabled'))
+                ]),
+                'tpl'       => HtmlGrid::img('{icon}', ['align' => 'absmiddle'], false) . ' {name}',
+                'filter'    => ['type' => 'string'],
+                'sortable'  => true,
+                'width'     => 200
+            ],
+            [
+                'text'      => $this->module->t('Name') . ' (' . Ge::$app->language->name . ')',
+                'dataIndex' => 'nameLo',
+                'cellTip'   => '{nameLo}',
+                'sortable'  => false,
+                'width'     => 270
+            ],
+            [
+                'text'      => '#Type',
+                'dataIndex' => 'type',
+                'cellTip'   => '{type}',
+                'filter'    => ['type' => 'string'],
+                'sortable'  => true,
+                'width'     => 180
+            ],
+            [
+                'xtype'       => 'g-gridcolumn-checker',
+                'text'        => ExtGrid::columnIcon('g-icon-m_backend', 'svg'),
+                'tooltip'     => '#Interface elements this field',
+                'collectData' => ['name'],
+                'dataIndex'   => 'isField',
+                'filter'    => ['type' => 'boolean']
+            ],
+            [
+                'xtype'       => 'g-gridcolumn-switch',
+                'text'        => ExtGrid::columnIcon('g-icon-m_visible', 'svg'),
+                'tooltip'     => '#Enabled',
+                'collectData' => ['name'],
+                'dataIndex'   => 'enabled',
+                'filter'    => ['type' => 'boolean']
+            ]
+        ];
+
+        // панель инструментов (Ge.view.grid.Grid.tbar GeJS)
+        $tab->grid->tbar = [
+            'padding' => 1,
+            'items'   => ExtGrid::buttonGroups([
+                'edit' => [
+                    'items' => [
+                        // инструмент "Добавить"
+                        'add' => [
+                            'caching' => false
+                        ],
+                        'delete',
+                        'cleanup',
+                        '-',
+                        'edit',
+                        'select',
+                        '-',
+                        'refresh'
+                    ]
+                ],
+                'columns',
+                'search'
+            ], [
+                'route' =>  Ge::alias('@route')
+            ])
+        ];
+
+        // контекстное меню записи (Ge.view.grid.Grid.popupMenu GeJS)
+        $tab->grid->popupMenu = [
+            'items' => [
+                [
+                    'text'    => '#Edit record',
+                    'iconCls' => 'g-icon-svg g-icon-m_edit g-icon-m_color_default',
+                    'handler' => 'loadWidget',
+                    'handlerArgs' => [
+                        'route'   => $this->module->route('/form/view/{id}'),
+                        'pattern' => 'grid.popupMenu.activeRecord'
+                    ]
+                ]
+            ]
+        ];
+
+        // 2-й клик по строке сетки
+        $tab->grid->rowDblClickConfig = [
+            'allow' => true,
+            'route' => $this->module->route('/form/view/{id}')
+        ];
+        // количество строк в сетке
+        $tab->grid->store->pageSize = 50;
+        // поле аудита записи
+        $tab->grid->logField = 'name';
+        // плагины сетки
+        $tab->grid->plugins = 'gridfilters';
+        // класс CSS применяемый к элементу body сетки
+        $tab->grid->bodyCls = 'g-grid_background';
+
+        // панель навигации (Ge.view.navigator.Info GeJS)
+        $tab->navigator->info['tpl'] = HtmlNav::tags([
+            HtmlNav::header('{nameLo}'),
+            HtmlNav::fieldLabel($this->t('Name'), '{name}'),
+            HtmlNav::fieldLabel(
+                $this->module->t('Name') . ' (' . Ge::$app->language->name . ')', 
+                '{nameLo}'
+            ),
+            HtmlNav::fieldLabel($this->t('Type'), '{type}'),
+            HtmlNav::fieldLabel(
+                ExtGrid::columnIcon('g-icon-m_backend', 'svg') . ' ' . $this->t('Form field'), 
+                HtmlNav::tplChecked('isField')
+            ),
+            HtmlNav::fieldLabel(
+                ExtGrid::columnIcon('g-icon-m_visible', 'svg') . ' '  . $this->t('Enabled'), 
+                HtmlNav::tplChecked('enabled')
+            ),
+            HtmlNav::widgetButton(
+                $this->t('Edit record'),
+                ['route' => $this->module->route('/form/view/{id}'), 'long' => true],
+                ['title' => $this->t('Edit record')]
+            )
+        ]);
+        $tab
+            ->addCss(RG_DEBUG ? '/grid.css' : '/grid.min.css')
+            ->addRequire('Ge.view.grid.column.Switch');
+        return $tab;
+    }
+}
